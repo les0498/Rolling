78c7aa0fb1566b6a18a785e6da3d9e806583cecc
geha <ospac111@gmail.com>
2025-07-10 23:51:54 +0900

    feat: Routes 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/list' element={<NotFound />} />
+        <Route path='/post' element={<NotFound />} />
+        <Route path='/post/:id' element={<NotFound />} />
+        <Route path='/post/:id/edit' element={<NotFound />} />
+        <Route path='/post/:id/message' element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
