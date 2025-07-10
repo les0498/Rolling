ac73cd5176d0196fa895bec72deb9b323fa9949e
geha <ospac111@gmail.com>
2025-07-10 23:00:24 +0900

    feat: _font.scss 생성, scss 전역 설정을 위한 세팅(vite.config.js, global.d.ts)

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,4 @@
-// CSS 모듈을 인식하도록 선언
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-// SCSS 모듈을 인식하도록 선언
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
+declare module '*.scss' {
+  const content: { [key: string]: any };
+  export = content;
 }
