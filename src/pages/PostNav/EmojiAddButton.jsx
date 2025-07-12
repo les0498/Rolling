d56c1dbf0646737c2e2f2acdb625db8d667353bd
Jiii-Eun <mh030231@gmail.com>
2025-07-12 18:45:21 +0900

    feat: tablet/mobile UI 추가

--- src/pages/PostNav/EmojiAddButton.jsx
+++ src/pages/PostNav/EmojiAddButton.jsx
@@ -1,13 +1,16 @@
 import addIcon from '@/assets/images/addIcon.png';
+import useIsMobile from '@/hooks/useIsMobile';
 import lineStyle from '@/pages/PostNav/AuthorCount.module.scss';
 import styles from '@/pages/PostNav/EmojiShareButton.module.scss';
 
 function EmojiAddButton() {
+  const isMobile = useIsMobile();
+
   return (
     <div className={styles.flexContainer}>
       <button className={styles.btn}>
         <img className={styles.icon} src={addIcon} alt='이모지추가아이콘' />
-        추가
+        {!isMobile && <span>추가</span>}
       </button>
       <div className={`${lineStyle.divider} ${lineStyle['dividerEmoji']}`} />
     </div>
