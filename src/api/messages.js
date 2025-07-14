d32a79fb90e373727e6033d7c4bedcd9094a2a7f
geha <ospac111@gmail.com>
2025-07-14 15:44:57 +0900

    feat: 메시지 api (messages.api) 추가

---
+++ src/api/messages.js
@@ -0,0 +1,53 @@
+import { BASE_API_URL } from '@/api/constants';
+import { customFetch } from '@/api/customFetch';
+
+const MESSAGE_RELATIONSHIP = {
+  친구: '친구',
+  지인: '지인',
+  동료: '동료',
+  가족: '가족',
+};
+const MESSAGE_FONT = {
+  'Noto Sans': 'Noto Sans',
+  Pretendard: 'Pretendard',
+  나눔명조: '나눔명조',
+  '나눔손글씨 손편지체': '나눔손글씨 손편지체',
+};
+const getMessagesById = async ({ id, limit = 8, offset = 0 }) => {
+  return await customFetch(
+    `${BASE_API_URL}recipients/${id}/messages/?limit=${limit}&offset=${offset}`
+  );
+};
+
+const createMessageById = async ({
+  id = '',
+  sender = '',
+  profileImageURL = '',
+  relationship = MESSAGE_RELATIONSHIP.지인,
+  content = '',
+  font = MESSAGE_FONT['Noto Sans'],
+}) => {
+  return await customFetch(`${BASE_API_URL}recipients/${id}/messages/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      sender,
+      profileImageURL,
+      relationship,
+      content,
+      font,
+    }),
+  });
+};
+const deleteMessageById = async (id) => {
+  return await customFetch(`${BASE_API_URL}messages/${id}/`, {
+    method: 'DELETE',
+  });
+};
+
+export {
+  MESSAGE_RELATIONSHIP,
+  MESSAGE_FONT,
+  getMessagesById,
+  createMessageById,
+  deleteMessageById,
+};
--- src/api/recipients.js
+++ src/api/recipients.js
@@ -1,17 +1,6 @@
 import { BASE_API_URL } from '@/api/constants';
 import { customFetch } from '@/api/customFetch';
 
-//대상 (Recipient): 게시판에 등록하는 롤링 페이퍼 대상 데이터를 저장합니다.
-
-const DEFAULT_RECIPIENT = {
-  id: 0,
-  name: '',
-  backgroundColor: '',
-  backgroundImageURL: null,
-  createdAt: '',
-  messageCount: 0,
-  recentMessages: [],
-};
 const BACKGROUND_COLOR = {
   beige: 'beige',
   purple: 'purple',
@@ -43,7 +32,6 @@ const deleteRecipientById = async (id) => {
 };
 
 export {
-  DEFAULT_RECIPIENT,
   BACKGROUND_COLOR,
   getRecipients,
   getRecipientById,
