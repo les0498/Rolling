634bb296e0a41629bcf864db3310566213a5a3a7
geha <ospac111@gmail.com>
2025-07-15 16:30:04 +0900

    feat: ProfileIcon 완료

--- src/components/ui/RelationBadge.jsx
+++ src/components/ui/RelationBadge.jsx
@@ -1,19 +1,19 @@
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 
 import { MESSAGE_RELATIONSHIP } from '@/api/message';
 import styles from '@/components/ui/RelationBadge.module.scss';
 
 export default function RelationBadge({ relationship }) {
-  const cx = classNames.bind(styles);
+  const cn = classNames.bind(styles);
   switch (relationship) {
     case MESSAGE_RELATIONSHIP.가족:
-      return <span className={cx('badge-family', 'badge')}>가족</span>;
+      return <span className={cn('badge-family', 'badge')}>가족</span>;
     case MESSAGE_RELATIONSHIP.동료:
-      return <span className={cx('badge-colleague', 'badge')}>동료</span>;
+      return <span className={cn('badge-colleague', 'badge')}>동료</span>;
     case MESSAGE_RELATIONSHIP.지인:
-      return <span className={cx('badge-acquaintance', 'badge')}>지인</span>;
+      return <span className={cn('badge-acquaintance', 'badge')}>지인</span>;
     case MESSAGE_RELATIONSHIP.친구:
-      return <span className={cx('badge-friend', 'badge')}>친구</span>;
+      return <span className={cn('badge-friend', 'badge')}>친구</span>;
     default:
       return;
   }
