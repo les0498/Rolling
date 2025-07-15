6728f582879b8066dfe9fbb7f1ad33246c0a5ecf
geha <ospac111@gmail.com>
2025-07-15 13:10:09 +0900

    refactor: RelationBadge에 classNames, bind 적용

--- src/components/ui/RelationBadge.jsx
+++ src/components/ui/RelationBadge.jsx
@@ -4,34 +4,16 @@ import { MESSAGE_RELATIONSHIP } from '@/api/message';
 import styles from '@/components/ui/RelationBadge.module.scss';
 
 export default function RelationBadge({ relationship }) {
-  console.log(relationship);
+  const cx = classNames.bind(styles);
   switch (relationship) {
     case MESSAGE_RELATIONSHIP.가족:
-      return (
-        <span className={classNames(styles['badge-family'], styles.badge)}>
-          가족
-        </span>
-      );
+      return <span className={cx('badge-family', 'badge')}>가족</span>;
     case MESSAGE_RELATIONSHIP.동료:
-      return (
-        <span className={classNames(styles['badge-colleague'], styles.badge)}>
-          동료
-        </span>
-      );
+      return <span className={cx('badge-colleague', 'badge')}>동료</span>;
     case MESSAGE_RELATIONSHIP.지인:
-      return (
-        <span
-          className={classNames(styles['badge-acquaintance'], styles.badge)}
-        >
-          지인
-        </span>
-      );
+      return <span className={cx('badge-acquaintance', 'badge')}>지인</span>;
     case MESSAGE_RELATIONSHIP.친구:
-      return (
-        <span className={classNames(styles['badge-friend'], styles.badge)}>
-          친구
-        </span>
-      );
+      return <span className={cx('badge-friend', 'badge')}>친구</span>;
     default:
       return;
   }
