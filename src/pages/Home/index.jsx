f76b495be8bf55cc5f6e8ead9de90fec6e6d5870
geha <ospac111@gmail.com>
2025-07-16 13:30:10 +0900

    fix: button disabled시에 focus, hover 색상 적용되는 문제 수정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,3 @@
-import Button from '@/components/ui/Button';
-import { BUTTON_VARIANT } from '@/components/ui/constants';
-
 export default function Home() {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '20px',
-        width: '120px',
-        margin: '150px 150px',
-      }}
-    >
-      <Button size='small' variant={BUTTON_VARIANT.primary}>
-        확인
-      </Button>
-      <Button size='big' variant={BUTTON_VARIANT.primary}>
-        확인
-      </Button>
-
-      <Button size='small' variant={BUTTON_VARIANT.secondary}>
-        확인
-      </Button>
-      <Button size='big' variant={BUTTON_VARIANT.secondary}>
-        확인
-      </Button>
-    </div>
-  );
+  return <div>home</div>;
 }
