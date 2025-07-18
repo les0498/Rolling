53094a2c224db5338badcb78a0c3156c4b72da19
suhyung kwon <ospac111@gmail.com>
2025-07-18 15:48:49 +0900

    refactor:  src/pages/list 삭제

--- src/pages/list/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-function SectionTitle({ children }) {
-  return (
-    <h2 style={{ fontSize: '24px', fontWeight: '700', marginBottom: '16px' }}>
-      {children}
-    </h2>
-  );
-}
-
-export default SectionTitle;
