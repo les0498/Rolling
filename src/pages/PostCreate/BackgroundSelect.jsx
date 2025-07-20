2c39089de95fd4aecbc282558d9c5ce345234f83
geha <ospac111@gmail.com>
2025-07-20 14:05:45 +0900

    feat: background boxes UI

--- src/pages/PostCreate/BackgroundSelect.jsx
+++ src/pages/PostCreate/BackgroundSelect.jsx
@@ -2,54 +2,76 @@ import { useState } from 'react';
 import classNames from 'classnames/bind';
 import { motion } from 'motion/react';
 
+import ClickedIcon from '@/assets/icons/enabled.svg';
 import styles from '@/pages/PostCreate/BackgroundSelect.module.scss';
 
 export default function BackgroundSelect() {
-  const [background, setBackground] = useState('컬러');
+  const [backgroundOption, setBackgroundOption] = useState('컬러');
+  const [background, setBackground] = useState('');
   const cx = classNames.bind(styles);
-  const onClickOption = (e) => {
+  const handleOptionSelect = (e) => {
     e.preventDefault();
-    setBackground(e.target.name);
+    setBackgroundOption(e.target.name);
   };
-  const isColorBackground = background === '컬러';
+  const handleBackgroundSelect = (e) => {
+    e.preventDefault();
+  };
+  const isColorBackgroundOption = backgroundOption === '컬러';
   return (
-    <section>
-      <h2 className={cx('label')}>배경화면을 선택해 주세요.</h2>
-      <p className={cx('caption')}>
-        컬러를 선택하거나, 이미지를 선택할 수 있습니다.
-      </p>
-      <motion.div className={cx('options-container')}>
+    <section className={cx('container')}>
+      <div className={cx('label-wrapepr')}>
+        <h2 className={cx('label')}>배경화면을 선택해 주세요.</h2>
+        <p className={cx('caption')}>
+          컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+        </p>
+      </div>
+      <div className={cx('options-wrapper')}>
         <motion.div
           className={cx('option-indicator')}
           layout
           style={{
-            left: isColorBackground ? '3px' : 'revert',
+            left: isColorBackgroundOption ? '3px' : 'revert',
           }}
         />
         <button
           name='컬러'
-          onClick={onClickOption}
+          onClick={handleOptionSelect}
           className={cx('option', 'option-color', {
-            'option-selected': isColorBackground,
-            'option-left': !isColorBackground,
+            'option-selected': isColorBackgroundOption,
+            'option-left': !isColorBackgroundOption,
           })}
         >
           컬러
         </button>
         <button
           name='이미지'
-          onClick={onClickOption}
+          onClick={handleOptionSelect}
           className={cx('option', {
-            'option-selected': !isColorBackground,
-            'option-right': isColorBackground,
+            'option-selected': !isColorBackgroundOption,
+            'option-right': isColorBackgroundOption,
           })}
         >
           이미지
         </button>
-      </motion.div>
-      <div>
-        <div>컬러</div>
-        <div>이미지</div>
+      </div>
+      <div className={cx('boxes-wrapper')}>
+        {isColorBackgroundOption ? (
+          <>
+            {Array.from({ length: 4 }, () => 0).map((box, i) => (
+              <button key={i} onClick={handleBackgroundSelect}>
+                <div className={cx('box', 'box-color')}></div>
+              </button>
+            ))}
+          </>
+        ) : (
+          <>
+            {Array.from({ length: 4 }, () => 0).map((box, i) => (
+              <button key={i} onClick={handleBackgroundSelect}>
+                <div className={cx('box', 'box-image')}></div>
+              </button>
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
