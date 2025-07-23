513c76a0e03533a45d9045a5b55a839065e75a4b
josubeen <whtnqls987@gmail.com>
2025-07-23 22:18:41 +0900

    feature: Message페이지 셀렉트바 커스터마이징

--- src/pages/message/RelationshipSelect.jsx
+++ src/pages/message/RelationshipSelect.jsx
@@ -1,26 +1,28 @@
+import Select from 'react-select';
+
 import styles from '@/pages/Message/RelationshipSelect.module.scss';
 
-function RelationshipSelect({ value, onChange }) {
-  const RELATIONSHIPS = ['친구', '지인', '동료', '가족'];
+const RELATIONSHIP_OPTIONS = [
+  { label: '친구', value: '친구' },
+  { label: '지인', value: '지인' },
+  { label: '동료', value: '동료' },
+  { label: '가족', value: '가족' },
+];
 
+function RelationshipSelect({ value, onChange }) {
   return (
     <div className={styles.selectWrapper}>
       <label htmlFor='relationship' className={styles.label}>
         상대와의 관계
       </label>
-      <select
-        id='relationship'
-        value={value}
-        //선택한 option의 value를 부모로 올려보냄
-        onChange={(e) => onChange(e.target.value)}
-        className={styles.select}
-      >
-        {RELATIONSHIPS.map((rel) => (
-          <option key={rel} value={rel}>
-            {rel}
-          </option>
-        ))}
-      </select>
+      <Select
+        inputId='relationship'
+        options={RELATIONSHIP_OPTIONS}
+        value={RELATIONSHIP_OPTIONS.find((opt) => opt.value === value)}
+        onChange={(selected) => onChange(selected.value)}
+        classNamePrefix='relationship'
+        isSearchable={false}
+      />
     </div>
   );
 }
