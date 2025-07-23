297ec3b6649d743c94b97aec0c75cea10b92ece0
Jiii-Eun <mh030231@gmail.com>
2025-07-23 12:41:18 +0900

    docs: 필요없는 문서 삭제

--- src/pages/Message/MessageForm.jsx
+++ src/pages/Message/MessageForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { createMessageById } from '@/apis/messages';
