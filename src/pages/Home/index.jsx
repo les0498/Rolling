8b82b3f9177219fe98300ccef7474f1521310bf1
geha <ospac111@gmail.com>
2025-07-24 11:41:07 +0900

    feat: layout padding 수정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
 import BottomCTA from '@/pages/Home/BottomCTA';
 import EmojiSection from '@/pages/Home/EmojiSection';
+import styles from '@/pages/Home/index.module.scss';
 import MainSection from '@/pages/Home/MainSection';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <MainSection />
       <EmojiSection />
       <BottomCTA />
-    </>
+    </div>
   );
 }
